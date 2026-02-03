713286429474394e3da82f664054280d337ea3f1
Deadfubs <deadfubs@gmail.com>
2026-02-03 19:51:50 -0300

    single post page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="home"),
     path("posts", views.posts, name="posts"),
-    path("posts/<slug>", views.post_datail, name="post-datails")
+    path("posts/<slug:slug>", views.post_details, name="post-details")
 ]
